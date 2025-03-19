c690918e69fc4df30299c770f78aa9abca3b8578
kagirimichael <michaelkagiri8@gmail.com>
2025-03-19 16:40:55 +0300

    io

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -6,10 +6,20 @@ export const axiosInstance = axios.create({
 });
 
 // Add a request interceptor to include the token
-axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token"); // Retrieve token from local storage
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`; // Set Authorization header
-    }
-    return config;
-});
+// axiosInstance.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("token"); // Retrieve token from local storage
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`; // Set Authorization header
+//     }
+//     return config;
+// });
+
+
+
+
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+//   withCredentials: true,
+// });
